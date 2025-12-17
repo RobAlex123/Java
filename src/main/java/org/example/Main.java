124a3bc5641e7f4dbb61be7efc34f629deb702ca
Roberto Blanco <raba_92@hotmail.com>
2025-12-17 06:40:17 -0600

    Project creation with example code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        String name = "";
+        String name = "Roberto";
 
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
@@ -14,7 +14,7 @@ public class Main {
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println(name +"i = " + i);
+            System.out.println(name +" ix  = " + i);
 
 
         }
